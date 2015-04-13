f8282ea110a1e7daf6851e24397de34a0076744c
Jorge Cisneros <fraxito@gmail.com>
2015-04-13 13:24:17 +0200

    Inicio
    Ahorcado con botones

---
+++ Ahorcado/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ahorcado
+{
+    public partial class Form1 : Form
+    {
+        //almacena la palabra que hay que adivinar
+        String palabraOculta = "CETYS";
+
+        //constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Todos los botones al hacer click vienen a este método
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //casteo el objeto a botón. Sólo va a poder ser botón porque 
+            //sólo se genera en los botones
+            Button miBoton = (Button)sender;
+
+            String letra = miBoton.Text;
+            letra = letra.ToUpper();
+
+            if (palabraOculta.Contains(letra)){
+                label1.Text = letra;
+            }
+        }
+    }
+}
